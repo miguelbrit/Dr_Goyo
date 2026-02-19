2a1e5ea90a027aefa376d05274fba32e9a277ab3
Miguel Brito <miguelbrit@gmail.com>
2026-02-19 00:04:06 -0400

    Fix: Remove importmap and import index.css in index.tsx to resolve grey screen

--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
